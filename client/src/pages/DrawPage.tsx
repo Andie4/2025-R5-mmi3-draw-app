505e8e82d6399d31ce991cb7ecaca786a41c959a
itsleaht <leaztanda@gmail.com>
2025-10-20 19:58:42 +0200

    init: first commit

---
+++ client/src/pages/DrawPage.tsx
@@ -0,0 +1,61 @@
+import { useEffect } from 'react'
+import { AppHeader } from '../components/AppHeader/AppHeader'
+import { DrawLayout } from '../components/DrawLayout/DrawLayout'
+import { DrawSocket } from '../DrawSocket'
+import { useMyUserStore } from '../store/useMyUserStore'
+import { createMyUser } from '../utils/create-my-user'
+import { Instructions } from '../components/Instructions/Instructions'
+import { getInstructions } from '../utils/get-instructions'
+
+function DrawPage() {
+  const setMyUser = useMyUserStore((state) => state.setMyUser)
+
+  const onClickJoin = () => {
+    DrawSocket.emit("myUser:join", createMyUser() );
+  }
+
+  useEffect(() => {
+    DrawSocket.listen("myUser:joined", (data) => {
+      setMyUser(data.user);
+
+      console.log("My User joined:success", data);
+    });
+    return () => {
+      DrawSocket.off("myUser:joined");
+    }
+  }, [setMyUser]);
+
+
+
+  return (
+    <DrawLayout
+      topArea={<AppHeader 
+        onClickJoin={onClickJoin}
+        
+      />}
+      rightArea={
+        <>
+          <Instructions>
+            {getInstructions('user-list')}
+          </Instructions>
+          {/* <!-- Ajouter le composant TestUserList ici --> */}
+          {/* <TestUserList /> */}
+        </>
+      }
+      bottomArea={
+        <>
+          <Instructions>
+            {getInstructions('toolbar')}
+          </Instructions>
+        </>
+      }
+    >
+      <Instructions>
+        {getInstructions('draw-area')}
+      </Instructions>
+      {/* <TestDrawArea /> */}
+    </DrawLayout>
+  )
+}
+
+export default DrawPage;
