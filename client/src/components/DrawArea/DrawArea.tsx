58f3bdbdf9327ccef2789f2c80565c7b272bde58
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-11-18 20:56:00 +0100

    test draw

--- client/src/components/DrawArea/DrawArea.tsx
+++ client/src/components/DrawArea/DrawArea.tsx
@@ -1,3 +1,4 @@
+import { log } from "console";
 import  { useCallback } from "react";
 
 type Props = {};
@@ -7,14 +8,8 @@ export const DrawArea = ({} :Props) => {
     // Handle mouse down event
   const onMouseDown = useCallback((event: React.MouseEvent<HTMLCanvasElement>) => {
     // canvas.drawing = true;
-    const d = document.getElementById("canvas");
-    const topPos = d.offsetTop;
-
-if (topPos > 400){
-    
-}
-
-
+    const canvas = event.currentTarget;
+    log(canvas);
     console.log("Mouse down at:", event.clientX, event.clientY);
     
   
